d8f3492b22bbb542a68a533ed65b2132f688ca09
heedoo moon <heedoo21c@gmail.com>
2020-03-26 00:32:55 -0500

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 import App from './App';
+import Home from './home/home';
+import Enzyme, {shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('should have Home component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(Home).length).toEqual(1);
 });
